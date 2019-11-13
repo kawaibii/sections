0c1495ccc55c4adeed8701e8f8662f29bca0dfdd
Khoan98 <khoanld98@gmail.com>
2019-11-13 15:04:37 +0700

    demov1

---
+++ src/sayhelo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kinn007
+ * Date: 11/13/2019
+ * Time: 3:02 PM
+ */
+
+namespace sections\HelloWorld;
+
+
+class sayhelo
+{
+    public function say(){
+        return "12345";
+    }
+}
